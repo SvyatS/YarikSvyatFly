69ccbce82140e305e57a80531dfaf7a99a007cb1
SvyatS <52736791+SvyatS@users.noreply.github.com>
2020-08-23 16:53:39 +0700

    update
    added bpla registration form and create profile params.

--- regBAS/views.py
+++ regBAS/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
+from .forms import *
 
 
 class RegistrationBPLA(View):
     def get(self, request):
-        return render(request, "regBAS/regBPLA.html")
+        form = RegistrationBPLAForm()
+        return render(request, "regBAS/regBPLA.html", {"form": form})
 
     def post(self, request):
-        return HttpResponse("<h4>Заявление на рассмотрении</h4>")
+        form = RegistrationBPLAForm(request.POST, request.FILES)
+        if form.is_valid():
+            owner = request.user
+            type = form.cleaned_data.get("type")
+            classification = form.cleaned_data.get("classification")
+            weight = form.cleaned_data.get("weight")
+            photo = form.cleaned_data.get("photo")
+            obj = Bas.objects.create(
+                owner = owner,
+                type = type,
+                classification = classification,
+                weight = weight,
+                photo = photo,
+                verification = "На проверке",
+            )
+            obj.save()
+            return HttpResponse("<h4>Заявление на рассмотрении</h4>")
+        else:
+            form = RegistrationBPLAForm()
+        return render(request, "regBAS/regBPLA.html", {"form": form})
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import regBAS.models as rb
 
 
 def home_page(request):
     return render(request, "users/home.html")
 
 def profile(request):
-    return render(request, "users/profile.html")
+    BPLA = rb.Bas.objects.filter(owner = request.user)
+    context = {"BPLA": BPLA}
+    return render(request, "users/profile.html", context)
